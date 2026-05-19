a1f47d6cce3bd207b7749a6ab933e68c842d44fa
Murilo Dark <murilo.dark@hotmail.com>
2026-05-19 08:25:06 -0300

    gitattributes incluso

--- api/database/seeders/DatabaseSeeder.php
+++ api/database/seeders/DatabaseSeeder.php
@@ -17,9 +17,13 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // ALTERADO: Procura pelo e-mail; se não existir, cria o usuário com o restante dos dados
+        User::firstOrCreate(
+            ['email' => 'test@example.com'],
+            [
+                'name' => 'Test User',
+                'password' => bcrypt('sua_senha_aqui'), // Lembre-se de definir uma senha se for testar o login depois
+            ]
+        );
     }
 }
